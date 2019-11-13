c132b4cd09e2eef54a4790ab6516831246b0ec44
Robert Beckett <robert.beckett@onion.red>
2019-11-13 02:19:18 +0000

    Implemented and tested getUserWithId

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -38,7 +38,6 @@ const getUserWithId = function(id) {
     WHERE id = $1;
   `, [id])
     .then(res => {
-      console.log(res.rows[0]);
       return null || res.rows[0];
     });
 };
@@ -51,11 +50,15 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+  return pool.query(`
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+  `, [user.name, user.email, user.password])
+    .then(res => {
+      return res.rows[0];
+    });
+};
 exports.addUser = addUser;
 
 /// Reservations
